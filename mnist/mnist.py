2d67146882d2b304c38543cf135c1e3ab4c58204
Ray Alez <raymestalez@gmail.com>
2016-08-23 04:18:19 -0400

    Update.

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -1,6 +1,7 @@
 # Plot ad hoc mnist instances
 from keras.datasets import mnist
 import matplotlib.pyplot as plt
+
 # load (downloaded if needed) the MNIST dataset
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 # plot 4 images as gray scale
